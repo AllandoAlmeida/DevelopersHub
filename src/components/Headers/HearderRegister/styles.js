32c06bbca53cd963cfcfb49c7ad949c5fc2f690e
Alando S Almeida Sobrinho <alandoalmeida0803@7j50z3.onmicrosoft.com>
2023-06-18 23:19:29 -0300

    estilização toastify

--- src/components/Headers/HearderRegister/styles.js
+++ src/components/Headers/HearderRegister/styles.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const StyledHeaderRegister = styled.header`
+  position: sticky;
+  background-color:#000000;
+  top:0;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   margin: 0 auto;
   padding: 0 1.2rem;
+
 
   div:nth-child(1) {
     display: flex;
